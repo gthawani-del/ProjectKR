e336d7dd55c655176ddd0fc239303aa5b19cc21a
gthawani-del <gthawani@gmail.com>
2026-09-06 17:17:51 +0530

    Use uploaded Krida Legal logo asset

--- components/SiteLogo.tsx
+++ components/SiteLogo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function SiteLogo({ className = '' }: { className?: string }) {
   return (
     <Link className={`siteLogo ${className}`} href="/" aria-label="Krida Legal home">
-      <Image src="/brand/krida-legal-logo.png" alt="Krida Legal" width={750} height={225} priority />
+      <Image src="/brand/krida-legal-logo.svg" alt="Krida Legal" width={560} height={137} priority />
     </Link>
   );
 }
